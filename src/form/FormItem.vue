3fd50887c1f13dc2327a4c254a1af88c43a193c9
sylvanas <jzm362466464@126.com>
2020-09-11 16:53:11 +0800

    create package file

--- src/form/FormItem.vue
+++ src/form/FormItem.vue
@@ -10,6 +10,7 @@
 </template>
 
 <script>
+import AsyncValidator from 'async-validator'
 export default {
   name: 'LgFormItem',
   props: {
@@ -20,9 +21,36 @@ export default {
       type: String
     }
   },
+  inject: ['form'],
   data () {
     return {
       errMessage: ''
+    }
+  },
+  mounted(){
+    // 渲染完毕，注册validate事件，内部调用validate方法
+    this.$on('validate', () => {
+      this.validate()
+    })
+  },
+  methods: {
+    validate () {
+      if(!this.prop) return
+      // form中验证规则和数据
+      const value = this.form.model[this.prop]
+      const rules = this.form.rules[this.prop]
+
+      // 验证的属性名及对象
+      const descriptor = { [this.prop]: rules }
+      const validator = new AsyncValidator(descriptor)
+
+      return validator.validate({ [this.prop]: value }, errors => {
+        if (errors) {
+          this.errMessage = errors[0].message
+        } else {
+          this.errMessage = ''
+        }
+      })
     }
   }
 }
--- src/form/Input.vue
+++ src/form/Input.vue
@@ -1,10 +1,41 @@
 <template>
-  <div></div>
+  <div>
+    <input v-bind="$attrs" :type="type" :value="value" @input="handleInput">
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'LgInput'
+  name: 'LgInput',
+  inheritAttrs: false,
+  props: {
+    value: {
+      type: String
+    },
+    type: {
+      type: String,
+      default: 'text'
+    }
+  },
+  methods: {
+    handleInput (evt) {
+      this.$emit('input', evt.target.value)
+      const findParent = parent => {
+        while (parent) {
+          if (parent.$options.name === 'LgFormItem') {
+            break
+          } else {
+            parent = parent.$parent
+          }
+        }
+        return parent
+      }
+      const parent = findParent(this.$parent)
+      if (parent) {
+        parent.$emit('validate')
+      }
+    }
+  }
 }
 </script>
 
