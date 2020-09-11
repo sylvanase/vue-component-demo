71482a8166c5d01eef3ebf88b086ab7a0cb3262e
sylvanas <jzm362466464@126.com>
2020-09-11 15:16:34 +0800

    init component demo

---
+++ src/form/Form.vue
@@ -0,0 +1,23 @@
+<template>
+  <form>
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'LgForm',
+  props: {
+    model: {
+      type: Object
+    },
+    rules: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/form/FormItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <div>
+      <slot ></slot> 
+      <!-- 显示验证失败的原因 -->
+      <p v-if="errMessage">{{ errMessage }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LgFormItem',
+  props: {
+    label: {
+      type: String
+    },
+    prop: {
+      type: String
+    }
+  },
+  data () {
+    return {
+      errMessage: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
