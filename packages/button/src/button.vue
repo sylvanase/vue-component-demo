54bdda1d35fceac63d5bd44e1f00c4e51593bd7e
sylvanas <jzm362466464@126.com>
2020-09-11 16:51:59 +0800

    package config

---
+++ packages/button/src/button.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="handleClick">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LgButton',
+  methods: {
+    handleClick(evt) {
+      this.$emit('click', evt)
+      // 阻止表单的默认事件
+      evt.preventDefault()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
